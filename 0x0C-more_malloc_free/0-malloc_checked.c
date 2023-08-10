2d297488f7fb1e05f785431a9d19c042b5df26b6
adedunni <adedunniayokunle@gmail.com>
2023-08-10 16:41:40 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* malloc_checked - if have error throw 98 error
-* @b: bytes
-* Return: pointer void
-*/
+**malloc_checked - allocate memory using malloc
+*
+*@b: unsigned int
+*
+*Return: the pointer
+**/
+
 void *malloc_checked(unsigned int b)
 {
-	void *point;
+	int *n;
 
-	point = malloc(b);
-	if (!point)
+	n = malloc(b);
+
+	if (!n)
+	{
+		free(n);
 		exit(98);
-	return (point);
+	}
+
+	return (n);
+
 }
